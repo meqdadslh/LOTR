8aa677186b7a9709e90aa535ab423108bb360950
Meqdad Ali <meqdadali@Meqdads-MacBook-Pro.local>
2021-04-11 23:11:33 -0400

    Chapter 13 complete -Gollum and the ring are gone, the baddies are done, and the hobbits are back in the shire

--- scripts/fellowship.js
+++ scripts/fellowship.js
@@ -299,10 +299,13 @@ const weWantsIt = () => {
 const thereAndBackAgain = () => {
 
     // 1. remove Gollum and the Ring from the DOM
+    $("#gollum").remove();
 
     // 2. remove all the baddies from the DOM
+    $(".baddies").remove();
 
     // 3. Move all the hobbits back to the shire
+    $(".hobbit").appendTo("#The-Shire")
 
 };
 
